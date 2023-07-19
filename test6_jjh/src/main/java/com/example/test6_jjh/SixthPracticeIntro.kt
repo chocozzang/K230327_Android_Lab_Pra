20237142bc7cd407d8eef597fe44c425bca34270
Jinho <jangjh_05@naver.com>
2023-07-19 16:44:55 +0900

    sixth practice test

---
+++ test6_jjh/src/main/java/com/example/test6_jjh/SixthPracticeIntro.kt
@@ -0,0 +1,19 @@
+package com.example.test6_jjh
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SixthPracticeIntro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sixth_practice_intro)
+
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this@SixthPracticeIntro, SixthPractice::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
